45af3541f08cbabf05ed26a70fdadcd2f152c121
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-09-19 13:38:07 -0300

    Criação do método get na rota /cars

--- src/controllers/Cars.controller.ts
+++ src/controllers/Cars.controller.ts
@@ -14,6 +14,12 @@ class CarController {
     return res.status(201).json(results);
   }
 
+  public async read(_req: Request, res: Response<ICar[]>) {
+    const results = await this._service.read();
+
+    return res.status(200).json(results);
+  }
+
   // public async readOne(
   //   req: Request,
   //   res: Response<ICar>,
